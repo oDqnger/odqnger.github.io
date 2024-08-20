ce297f0ad08cfa9a6409be8e0a7597b352ac2921
oDqnger <dimanchansilu@gmail.com>
2024-08-20 17:52:18 +0000

    added radix-ui component library instead

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import Root from "./routes/root.tsx"
 import ErrorPage from "./routes/error-page.tsx"
 import "./styles/output.css"
+import "@radix-ui/themes/styles.css"
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { Theme } from "@radix-ui/themes"
 
 const router = createBrowserRouter([
   {
@@ -25,6 +27,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Theme>
+      <RouterProvider router={router} />
+    </Theme>
   </StrictMode>,
 )
--- app/src/routes/root.tsx
+++ app/src/routes/root.tsx
@@ -1,10 +1,11 @@
-import "../styles/output.css"
+import { Button } from "@radix-ui/themes"
 
 function Root() {
  
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-red-400 border-red-500">Hello World!</h1>
+      <Button>HEllo</Button>
     </>
   )
 }
