c11e7b262d22a1ef545e5fcb9a9d082cc756ae65
oDqnger <dimanchansilu@gmail.com>
2024-08-24 10:42:53 +0000

    added links and made the site a bit more better looking

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -4,10 +4,10 @@ function NavBar(props: any) {
   return (
     <>
       <div data-theme="dim" className={props.className + " mr-52 ml-52 rounded-full text-center navbar-center bg-base-100"}>
-        <Link to="/" className="btn btn-ghost text-lg pr-5 font-main2">Home</Link>
-        <Link to="/" className="btn btn-ghost text-lg pr-5 font-main2">Projects</Link>
-        <Link to="/" className="btn btn-ghost text-lg pr-5 font-main2">Skills</Link>
-        <Link to="/" className="btn btn-ghost text-lg pr-5 font-main2">Contact Me</Link>
+        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">Home</Link>
+        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">Projects</Link>
+        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">Skills</Link>
+        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">Contact Me</Link>
       </div>
     </>
   )
--- app/src/routes/root.tsx
+++ app/src/routes/root.tsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom"
 import NavBar from "../components/NavBar.tsx"
-import pfp from "../assets/pfp.jpg"
+import { FaDiscord, FaGithub } from "react-icons/fa";
 
 function Root() {
  
   return (
     <>
       <section data-theme="dim" className="font-main2 h-screen bg-cover bg-center">
-        <div className="pt-5 h-full">
+        <div className="text-center pt-5 h-full">
           <NavBar />
 
           <div className="avatar flex items-center justify-center mt-52">
@@ -15,11 +16,13 @@ function Root() {
             </div>
           </div>
 
-          <h1 className="h-50 font-bold font-main2 text-white text-6xl text-center mt-5">odqnger the shitty web developer</h1>
+          <h1 className="h-50 font-bold font-main2 text-white text-6xl text-center mt-5">diman the idiot</h1>
+          <Link className="transition ease-in-out hover:scale-110 text-6xl mr-6 mt-7 inline-block" to="https://discord.com/users/794023970474819585"><FaDiscord /></Link>
+          <Link className="transition ease-in-out hover:scale-110 text-6xl mt-7 inline-block" to="https://github.com/oDqnger"><FaGithub /></Link>
         </div>
       </section>
-      <section>
-
+      <section data-theme="dim" className="font-main2">
+        <h1>Projects</h1>
       </section>
     </>
   )
