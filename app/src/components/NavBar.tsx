447d9c764d7e810b138ceefa5b931a3cc0bff3ed
oDqnger <dimanchansilu@gmail.com>
2024-08-30 16:29:08 +0000

    done the website frfr

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,13 +1,9 @@
-import { Link } from "react-router-dom"
-
 function NavBar(props: any) {
   return (
     <>
       <div data-theme="dim" className={props.className + " mr-52 ml-52 rounded-full navbar-center bg-base-100"}>
-        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">home</Link>
-        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">projects</Link>
-        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">skills</Link>
-        <Link to="/" className="btn btn-ghost text-base pr-5 font-main2">contact me</Link>
+        <a href="#home" className="btn btn-ghost text-base pr-5 font-main2">home</a>
+        <a href="#projects" className="btn btn-ghost text-base pr-5 font-main2">projects</a>
       </div>
     </>
   )
--- app/src/routes/root.tsx
+++ app/src/routes/root.tsx
@@ -8,36 +8,38 @@ import {
   FaPython, 
   FaLinux } 
 from "react-icons/fa";
+import pfp from "../assets/pfp.jpg"
 
 function Root() {
  
   return (
     <>
-      <section data-theme="dim" className="font-main2 h-screen bg-cover bg-center">
+      <section id="home" data-theme="dim" className="font-main2 h-screen bg-cover bg-center">
         <div className="text-center pt-5 h-full">
           <NavBar />
 
-          <div className="avatar flex items-center justify-center mt-52">
+          <div className="transition ease-in-out hover:scale-125 avatar inline-block items-center justify-center mt-52">
             <div className="w-32 rounded-full">
-              <img src="https://cdn.discordapp.com/avatars/794023970474819585/09fbf814391e284a09fc476eb9e48b58.webp?size=80" />
+              <img src={pfp} />
             </div>
           </div>
 
-          <h1 className="h-50 font-bold text-6xl mt-5">diman the idiot</h1>
+          <h1 className="h-50 font-bold text-6xl mt-5">odqnger the idiot</h1>
+          <h2 className="h-50 font-medium text-2xl mt-2">i just like to code and game, thats all i do</h2>
           <Link className="transition ease-in-out hover:scale-110 text-6xl mr-6 mt-7 inline-block" to="https://discord.com/users/794023970474819585"><FaDiscord /></Link>
 
           <Link className="transition ease-in-out hover:scale-110 text-6xl mt-7 inline-block" to="https://github.com/oDqnger"><FaGithub /></Link>
         </div>
       </section>
 
-      <section id="#projects" data-theme="emerald" className="font-main2 content-center text-center">
+      <section id="projects" data-theme="emerald" className="font-main2 content-center text-center">
         <h1 className="h-50 font-bold text-6xl mt-14 mb-14">projects ive made</h1>
         <div className="mb-14">
           <ProjectCard title="LumosUI" description="A react UI component library" link_names={["https://github.com/oDqnger/LumosUI", "https://www.npmjs.com/package/@odqnger/lumosui"]} links={["github", "npm"]} logo={<FaReact />} />
 
           <ProjectCard title="utilitilib" description="A python utility library" link_names={["https://github.com/oDqnger/utilitilib", "https://pypi.org/project/utilitilib/"]} links={["github", "pypi"]} logo={<FaPython />} />
 
-          <ProjectCard title="diman.dev" description="My personal website" link_names={["https://github.com/oDqnger"]} links={["github"]}/>
+          <ProjectCard title="odqnger.dev" description="My personal website" link_names={["https://github.com/oDqnger"]} links={["github"]}/>
         </div>
         <div className="mb-14">
           <ProjectCard title="chat-spammer" description="An app that spams messages" link_names={["https://github.com/oDqnger/chat-spammer"]} links={["github"]} logo={<FaPython />} />
